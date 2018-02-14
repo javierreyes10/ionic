7b30a799a9951205135535876baa5b2dac649ce1
Javier Reyes <reyesj02@stepstone.com>
2018-02-14 13:30:05 -0600

    Added the getTournamentData() method

--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {TeamHomePage} from '../pages'
+import { EliteApi } from '../../shared/shared'
 
 /**
  * Generated class for the TeamsPage page.
@@ -16,17 +17,23 @@ import {TeamHomePage} from '../pages'
 })
 export class TeamsPage {
 
+teams = [];
+/*
   teams = [
     {id: 1, name: 'HC Elite'},
     {id: 2, name: 'Team Takeover'},
     {id: 3, name: 'DC Thunder'}
   ];
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+*/
+  constructor(public navCtrl: NavController, public navParams: NavParams, public eliteApi: EliteApi) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TeamsPage');
+    let selectedTourney = this.navParams.data;
+    this.eliteApi.getTournamentData(selectedTourney.id).subscribe(data =>{
+      this.teams = data.teams;
+    })
   }
 
   itemTapped($event, team){
--- src/shared/elite-api.service.ts
+++ src/shared/elite-api.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core'
 import { Http, Response } from '@angular/http'
 import { Observable } from "rxjs/Observable";
+import 'rxjs';
 
 @Injectable()
 export class EliteApi{
     
     private baseUrl = 'https://elite-schedule-app-i2-b3b76.firebaseio.com'; 
+    currentTourney: any = {};
+
     constructor(public http: Http) {}
 
     getTournaments(): Observable<any>{
         return this.http.get(this.baseUrl+'/tournaments.json');
     }
+
+    getTournamentData(tourneyId) : Observable<any>{
+        return this.http.get(`${this.baseUrl}/tournaments-data/${tourneyId}.json`)
+        .map((response: Response) =>{
+            this.currentTourney = response.json();
+            return this.currentTourney;
+        });
+    }
+
 }
